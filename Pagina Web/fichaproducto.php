94d84bbbc4ee66f3a3c2cd1a106c09c4c06067a2
LucasHerrero <106039254+LucasHerrero@users.noreply.github.com>
2023-12-01 08:47:01 -0300

    Creacion de tabla en ficha producto html y scss
    falta formulario de compra y revisar tarea para mas. Pagina de referencia: https://serseo.es/tabla-tarifas-precios-servicios-marketing-digital-pymes/

---
+++ Pagina Web/fichaproducto.php
@@ -0,0 +1,146 @@
+<?php require("views/parts/header.php"); ?>
+
+
+
+
+
+    <section id="nuestros-programas">
+        <div class="container">
+         
+            <div class="programas">
+                <div class="carta1">
+                    <h3>Paquete basico</h3>
+                    <h4>9.99 € / mes</h4>
+                    <p>Este paquete trae incluido:</p>
+                    <ul>
+                    <li>+ Atencion al cliente 24/7</li>
+                    <li>+ Gestion de redes sociales</li>
+                    <li>+ Seo de sus paginas web</li>
+                    </ul>
+                    <div class="btnPos">
+                    <a href="#comparative-table" class="btn colored-button" id="button">
+                    <span class="face">+info</span>
+                    </a>
+                    </div>
+                </div>
+                <div class="carta2">
+                <h3>Paquete medio</h3>
+                    <h4>19.99 € / mes</h4>
+                    <p>Este paquete trae incluido:</p>
+                    <ul>
+                    <li>+ Atencion al cliente 24/7</li>
+                    <li>+ Gestion de redes sociales</li>
+                    <li>+ Creacion de pagina Web</li>
+                    <li>+ Seo de sus paginas web</li>
+                    
+                    </ul>
+                    <div class="btnPos2">
+                    <a href="#comparative-table" class="btn colored-button" id="button">
+                    <span class="face">+info</span>
+                    </a>
+</div>
+                </div>
+                <div class="carta3">
+                <h3>Paquete premiun</h3>
+                    <h4>59.99 € / mes</h4>
+                    <p>Este paquete trae incluido:</p>
+                    <ul>
+                    <li>+ Atencion al cliente 24/7</li>
+                    <li>+ Gestion de redes sociales</li>
+                    <li>+ Creacion de pagina Web</li>
+                    <li>+ Seo de sus paginas web</li>
+                    <li>+ Mantenimiento de la pagina web</li>
+                    <li>+ Creacion de aplicacion de venta</li>
+                    <li>+ Servicio personalizado</li>
+                    </ul>
+                    <div class="btnPos3">
+                    <a href="#comparative-table" class="btn colored-button" id="button">
+                    <span class="face">+info</span>
+                    </a>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="comparative-table">
+<div class="Table">
+
+    <table>
+        <th class="titcol">
+         
+        </th>
+        <th class="basico">
+            
+        Paquete basico
+            </th >
+        <th class="medio">
+        Paquete medio
+        </th>
+        <th class="premiun">
+        Paquete premiun
+        </th>
+
+        <tr>
+            <td class="titcol">Atencion al cliente</td>
+           <td class="basico"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+           <td class="medio"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+           <td class="premiun"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+            
+        </tr>
+        <tr>
+        <td class="titcol">Gestion de redes sociales</td>
+          
+        <td class="basico"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+        <td class="medio"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+        <td class="premiun"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+            
+        </tr>
+        <tr>
+        <td class="titcol">Seo de tus paginas web</td>
+         
+        <td class="basico"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+        <td class="medio"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+        <td class="premiun"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+         
+        </tr>
+        <tr>
+        <td class="titcol">Creacion de sus paginas web</td>
+        
+        <td class="basico"><img src="Static/img/cancel.png" alt="" class="Icon"></td>
+            <td class="medio"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+            <td class="premiun"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+            
+        </tr>
+        <tr>
+        <td class="titcol">Mantenimiento de la pagina web</td>
+
+        <td class="basico"><img src="Static/img/cancel.png" alt="" class="Icon"></td>
+        <td class="medio"><img src="Static/img/cancel.png" alt="" class="Icon"></td>
+        <td class="premiun"><img src="Static/img/checked.png" alt="" class="Icon"></td>
+          
+        </tr>
+        <tr>    
+            <td class="titcol">Mejor calificacion</td>
+       
+            <td class="basico"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"></td>
+            <td class="medio"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"></td>
+            <td class="premiun"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"><img src="Static/img/star.png" alt="" width="20px"></td>
+           
+        </tr>
+        <tr class="precio">
+        <td class="titcol">Precio</td>
+        
+            <td class="basico">9.99€ /mes <span>por mes</span> <button class="BtnCompraFinal">¡ Compra aquí !</button></td>
+            <td class="medio">19.99€ /mes <span>por mes</span><button class="BtnCompraFinal">¡ Compra aquí !</button></td>
+            <td class="premiun">59.99€ /mes <span>por mes</span> <button class="BtnCompraFinal">¡ Compra aquí !</button></td>
+             
+        </tr>
+    </table>
+</div>
+</section>
+    
+
+
+
+<?php require("views/parts/footer.php"); ?>
